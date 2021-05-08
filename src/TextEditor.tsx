ddbf6669fc1494d8eebdf307b3c3000c1bf55643
Jimmy <jimmylanbcn@gmail.com>
2021-05-08 14:38:52 -0400

    Prevent multiple instances of toolbar

--- src/TextEditor.tsx
+++ src/TextEditor.tsx
@@ -3,7 +3,7 @@
  * Creation Date: 2021-05-08
  */
 
-import React, { FunctionComponent, useEffect } from "react";
+import React, { FunctionComponent, useCallback } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
@@ -12,11 +12,18 @@ interface OwnProps {}
 type Props = OwnProps;
 
 const TextEditor: FunctionComponent<Props> = (props) => {
-  useEffect(() => {
-    new Quill("#container", { theme: "snow" });
+  // A ref to wrapper of this editor
+  const wrapperRef = useCallback((wrapper: HTMLDivElement | null) => {
+    if (!wrapper) {
+      return;
+    }
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    new Quill(editor, { theme: "snow" });
   }, []);
 
-  return <div id="container"></div>;
+  return <div id="container" ref={wrapperRef} />;
 };
 
 export { TextEditor };
