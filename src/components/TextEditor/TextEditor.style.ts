f9b8e6c0275a9255f457943a8a3e4de0904fe5fd
Jimmy <jimmylanbcn@gmail.com>
2021-05-09 12:28:05 -0400

    Add editor styles

--- src/components/TextEditor/TextEditor.style.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Created by Jimmy Lan
- * Creation Date: 2021-05-09
- */
-
-import { createStyles, withStyles } from "@material-ui/core";
-
-export const useStyles = createStyles(withStyles({}));
