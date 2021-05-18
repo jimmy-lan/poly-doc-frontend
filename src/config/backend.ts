e1f6d8d797b09f6edb2269cdbdd9741bb62f8791
Jimmy <jimmylanbcn@gmail.com>
2021-05-17 22:37:20 -0400

    Add config file

---
+++ src/config/backend.ts
@@ -0,0 +1,8 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2021-05-17
+ */
+
+export const backendConfig = {
+  domain: "http://localhost:5000",
+};
---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2021-05-17
+ */
+
+export * from "./backend";
